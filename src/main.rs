817ea166fb07d7bbe55061526e28f7b212d266a8
Elton Campos <eltoncampos36@gmail.com>
2023-02-06 12:11:18 -0300

    tuple

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,10 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-  let arr_1 = [1,2,3,4,5,6,7,8,9];
+  let my_tp: (u8, String, f64) = (47, "foo".to_string(), 50_000.00); 
 
-  let mut idx = 0;
+  println!("Name: {}", my_tp.1);
+  let(v1, v2, v3) = my_tp;
+  println!("Age: {}", v1);
 
-  for val in arr_1.iter() {
-    println!("val: {}", val)
-  }   
 }
