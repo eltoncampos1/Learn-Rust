312296ca3cf6ade9543443bd7d9c5a06cd158e82
Elton Campos <eltoncampos36@gmail.com>
2023-02-06 17:27:56 -0300

    Iterators

--- src/main.rs
+++ src/main.rs
@@ -10,31 +10,11 @@ use std::collections::HashMap;
 
 
 fn main() {
-     let path = "lines.txt";
-     let output = File::create(path);
-     let mut output = match output {
-         Ok(file) => file,
-         Err(error) => {
-            panic!("Problem creating file: {:?}", error);
-         }
-     };
-     write!(output, "Just some\nRandom words").expect("Failed to write to file");
-     let input = File::open(path).unwrap();
-     let buffered = BufReader::new(input);
-     for line in buffered.lines()
-     {
-        println!("{}", line.unwrap());
-     }
+   let mut arr_it = [1,2,3,4,5];
+   for val in arr_it.iter() {
+    println!("{}", val);
+   }
 
-     let output2 = File::create("rand.txt");
-     let output2 = match output2 {
-         Ok(file) => file,
-         Err(error) => match error.kind() {
-             ErrorKind::NotFound => match File::create("rand.txt") {
-                 Ok(fc) => fc,
-                 Err(err) => panic!("cant create file: {:?}", err),
-             },
-             _other_error => panic!("Problem opening file: {:?}", error)
-         }
-     };
+   let mut iter1 = arr_it.iter();
+   println!("1st: {:?}", iter1.next());
 }
