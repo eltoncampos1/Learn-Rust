e233ae1839f51490336e861e2beef41a44c03c70
Elton Campos <eltoncampos36@gmail.com>
2023-02-06 12:06:16 -0300

    loop

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,21 @@ use std::cmp::Ordering;
 
 fn main() {
   let arr_1 = [1,2,3,4,5,6,7,8,9];
-  println!("First: {}", arr_1[0]);
-  println!("length: {}", arr_1.len());
+
+  let mut idx = 0;
+
+  loop {
+    if(arr_1[idx] % 2 == 0) {
+        idx +=1;
+        continue;
+    }
+
+    if(arr_1[idx] == 9) {
+        break;
+    }
+
+    println!("Val: {}", arr_1[idx]);
+    idx +=1;
+  }
     
 }
