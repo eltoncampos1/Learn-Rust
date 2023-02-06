ad6b0e67256ef9f16967ca9499fe5ae63da14dd2
Elton Campos <eltoncampos36@gmail.com>
2023-02-06 11:43:46 -0300

    max int

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,10 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-    const ONE_MIL: u32 = 1_000_000;
-    const PI: f32 = 3.141592;
-    let age = "25";
-    let mut age: u32 = age.trim().parse()
-        .expect("Age wasen't assigned a number");
-    age = age +1;
-    println!("I'm {} and i Want ${}", age, ONE_MIL)
+ println!("Max u32: {}", u32::MAX);
+ println!("Max u64: {}", u64::MAX);
+ println!("Max usize: {}", usize::MAX);
+ println!("Max u128: {}", u128::MAX);
+ println!("Max f32: {}", f32::MAX);
+ println!("Max f64: {}", f64::MAX);
 }
