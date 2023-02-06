2c61ca9755f4f49f05b2f59d0743dbff82a7c3aa
Elton Campos <eltoncampos36@gmail.com>
2023-02-06 11:44:56 -0300

    bool and char

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,6 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
- println!("Max u32: {}", u32::MAX);
- println!("Max u64: {}", u64::MAX);
- println!("Max usize: {}", usize::MAX);
- println!("Max u128: {}", u128::MAX);
- println!("Max f32: {}", f32::MAX);
- println!("Max f64: {}", f64::MAX);
+    let is_true = true;
+    let my_grade = 'A';
 }
