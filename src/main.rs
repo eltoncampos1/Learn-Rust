5f10a01e19e93bf69801e82e930d6a33b90ee6a8
Elton Campos <eltoncampos36@gmail.com>
2023-02-06 12:55:14 -0300

    generics

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,17 @@
 #![allow(unused)]
 
 use std::io;
+use std::ops::Add;
 use rand::Rng;
 use std::io::{Write, BufReader, BufRead, ErrorKind};
 use std::fs::File;
 use std::cmp::Ordering;
 
-fn sum_list(list: &[i32]) -> i32 {
-    let mut sum = 0;
-    for &val in list.iter() {
-        sum += &val;
-    }
-
-    return sum;
+fn get_sum_gen<T: Add<Output = T>>(x: T, y: T) -> T {
+    return x + y;
 }
 
 fn main() {
-   let list = vec![1,2,3,4,5];
-   println!("Sum: {}", sum_list(&list))
+   println!("Sum: {}", get_sum_gen(5.3, 4.8));
+   println!("Sum: {}", get_sum_gen(5, 4));
 }
