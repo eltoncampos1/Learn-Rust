6da0d0be04f033f49b6a2b1529d8a083ca4b60d7
Elton Campos <eltoncampos36@gmail.com>
2023-02-06 19:11:09 -0300

    rc T

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,40 @@ use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::thread;
 use std::time::Duration;
+use std::cell::RefCell;
+use std::sync::{Arc, Mutex};
 
 
 fn main() {
-    let th1 = thread::spawn(|| {
-        for i in 1..25 {
-            println!("Spawned thread: {}", i);
-            thread::sleep(Duration::from_millis(1));
-        }
-    });
-
-    for i in 1..20 {
-        println!("Main thread : {}", i);
-        thread::sleep(Duration::from_millis(1));
+    pub struct Bank {
+        balance: f32
     }
 
-    th1.join().unwrap();
+    fn withdraw(the_bank: &Arc<Mutex<Bank>>, amt: f32) {
+        let mut bank_ref = the_bank.lock().unwrap();
+        if bank_ref.balance < 5.00 {
+            println!("Current balance : {} withdraw a smaller amount.", bank_ref.balance);
+        } else {
+            bank_ref.balance -= amt;
+            println!("Customer withdraw {} Current balance: {}", amt, bank_ref.balance);
+        }
+    }
+
+    fn customer(the_bank: Arc<Mutex<Bank>>) {
+        withdraw(&the_bank, 5.00);
+    }
+
+    let bank: Arc<Mutex<Bank>> = Arc::new(Mutex::new(Bank{balance: 20.00}));
+
+    let handles = (0..10).map(|_| {
+        let bank_ref = bank.clone();
+        thread::spawn(|| {
+            customer(bank_ref);
+        })
+    });
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Total {}", bank.lock().unwrap().balance);
 }
