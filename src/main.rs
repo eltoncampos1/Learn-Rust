bc0b000a79fa11056b7a881f866d4e81175a311f
Elton Campos <eltoncampos36@gmail.com>
2023-02-06 12:22:14 -0300

    strings, pt2

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,25 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
- let mut st1 = String::new();
- st1.push('A');
- st1.push_str(" word");
- for word in st1.split_whitespace() {
-    println!("{}", word);
+ let st1 = String::from("a f j o u r d n l mm b");
+ let mut v1: Vec<char> = st1.chars().collect();
+ v1.sort();
+ v1.dedup();
+ for char in v1 {
+    println!("{}", char);
  }
 
- let st2 = st1.replace("A", "Another");
- println!("{}", st2);
+ let st2: &str = "Ramdom string";
+ let mut str3: String = st2.to_string();
+ println!("{}", str3);
+ let  byte_arr1 = str3.as_bytes();
+ let st4 = &str3[0..6];
+ println!("String length: {}", st4.len());
+ str3.clear();
+ let st4  = String::from("Just SOme");
+ let st5  = String::from("worss");
+ let st6 = st4 + &st5;
+ for char in st6.bytes() {
+    println!("Char {}", char)
+ }
 }
