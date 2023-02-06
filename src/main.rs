1eb274103780d217a13adee9394b17a649653711
Elton Campos <eltoncampos36@gmail.com>
2023-02-06 11:55:31 -0300

    ternary op

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,12 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-   let age = 8;
-   if (age >= 1) && (age <= 18) {
-    println!("Important Birthday");
-   } else if (age == 32) || (age == 50) {
-    println!("Important Birthday");
+  let mut my_age = 25;
+  let can_vote = if my_age >= 18{
+    true
+  } else {
+    false
+  };
 
-   } else if (age >= 65) {
-    println!("Important Birthday");
-   } else {
-    println!("Not Important Birthday");
-
-   }
-
+  println!("Can vote: {}", can_vote)
 }
