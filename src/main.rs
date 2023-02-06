d3947456ea2f66c2232a2bc4fe196769c8e57ab5
Elton Campos <eltoncampos36@gmail.com>
2023-02-06 18:40:15 -0300

    thread, p1

--- src/main.rs
+++ src/main.rs
@@ -7,30 +7,22 @@ use std::io::{Write, BufReader, BufRead, ErrorKind};
 use std::fs::File;
 use std::cmp::Ordering;
 use std::collections::HashMap;
+use std::thread;
+use std::time::Duration;
 
 
 fn main() {
-    struct TreeNode<T> {
-        pub left: Option<Box<TreeNode<T>>>,
-        pub right: Option<Box<TreeNode<T>>>,
-        pub key: T,
+    let th1 = thread::spawn(|| {
+        for i in 1..25 {
+            println!("Spawned thread: {}", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..20 {
+        println!("Main thread : {}", i);
+        thread::sleep(Duration::from_millis(1));
     }
 
-    impl<T> TreeNode<T> {
-        pub fn new(key: T) -> Self {
-            TreeNode { left: None, right: None, key }
-        }
-        pub fn left(mut self, node: TreeNode<T>) -> Self {
-            self.left = Some(Box::new(node));
-            self
-        }
-        pub fn right(mut self, node: TreeNode<T>) -> Self {
-            self.right = Some(Box::new(node));
-            self
-        }
-
-    }
-
-    let node1 = TreeNode::new(1).left(TreeNode::new(2)).right(TreeNode::new(3));
-    
+    th1.join().unwrap();
 }
