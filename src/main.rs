3736816ec8e2d2f5303621aed9b2d73012387596
Elton Campos <eltoncampos36@gmail.com>
2023-02-06 15:00:46 -0300

    Traits

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,36 @@ use std::cmp::Ordering;
 use std::collections::HashMap;
 
 fn main() {
-   struct Customer {
-    name: String,
-    address: String,
-    balance: f32,
-   }
+    const PI: f32 = 3.141592;
+    trait Shape {
+        fn new(length: f32, width: f32) -> Self;
+        fn area(&self) -> f32;
+    }
 
-   let mut bob = Customer{
-    name: String::from("Bob"),
-    address: String::from("555 main st"),
-    balance: 234.67
-   };
+    struct Rectangle {length: f32, width: f32};
+    struct Circle {length: f32, width: f32};
 
-   bob.address = String::from("505 main st");
-   
+    impl Shape for Rectangle {
+        fn new(length: f32, width: f32) -> Rectangle {
+            return Rectangle {length, width}
+        }
+        fn area(&self) -> f32 {
+            return self.length * self.width;
+        }
+    }
+    impl Shape for Circle {
+        fn new(length: f32, width: f32) -> Circle {
+            return Circle {length, width}
+        }
+        fn area(&self) -> f32 {
+            return (self.length / 2.0).powf(2.0) * PI;
+        }
+    }
+
+    let rec: Rectangle = Shape::new(10.0, 10.0);    
+
+    let circ: Circle = Shape::new(10.0, 10.0);    
+    println!("REC area: {}", rec.area());
+    println!("CIRC area: {}", circ.area());
+
 }
