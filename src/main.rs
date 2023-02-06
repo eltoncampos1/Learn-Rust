2a4d76f9c9ffc9832c109f5ffec256a741d0423d
Elton Campos <eltoncampos36@gmail.com>
2023-02-06 11:58:17 -0300

    match op, pt1

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,11 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-  let mut my_age = 25;
-  let can_vote = if my_age >= 18{
-    true
-  } else {
-    false
+  let age2 = 8;
+  match age2 {
+    1..=18 => println!("Important Bday"),
+    21 | 50 => println!("Important Bday"),
+    65..=i32::MAX => println!("Important Bday"),
+    _ => println!("NOT an Important Bday"),
   };
-
-  println!("Can vote: {}", can_vote)
 }
